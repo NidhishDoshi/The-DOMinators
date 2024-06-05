4778bb958c290d9cb4d16c6b746999308dabc578
NidhishDoshi <nidhishadoshi05@gmail.com>
2024-06-05 17:29:04 +0530

    Initial Login Page

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import pg from "pg";
+
+const db=new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "The DOMinators",
+    password: "nidhish3",
+    port: 5432
+});
+const __dirname=dirname(fileURLToPath(import.meta.url));
+const port=3000;
+const app=express();
+var response="";
+db.connect();
+
+app.use(express.static('assets'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",(req,res)=>{
+    res.render(__dirname+"/views/login.ejs",{
+        response: response,
+    });
+});
+
+app.post("/login",async (req,res) =>{
+    const username=req.body.Username;
+    const password=req.body.Password;
+    try
+    {
+        const result=await db.query("SELECT password FROM users WHERE name=$1",[username]);
+        const pass=result.rows[0].password;
+        if(password===pass)
+        {
+            response="Login Successful.";
+            res.redirect("/");
+        }
+        else{
+            response="Username and Password does not match. Try Again.";
+            res.redirect("/");
+        }
+    }
+    catch(err){
+        console.error("Error: ",err.message);
+        res.redirect("/");
+    }
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}.`)
+});
