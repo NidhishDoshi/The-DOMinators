8e235d4aef5e3c1d690c9f76329c448987c9cd37
NidhishDoshi <140102883+NidhishDoshi@users.noreply.github.com>
2024-06-10 13:52:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,54 +3,87 @@ import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import mysql from "mysql2";
-import env from "dotenv";
+import bcrypt from "bcrypt";
 
-env.config();
-const db1=await mysql.createConnection({
+const db=mysql.createConnection({
     host: "sql7.freesqldatabase.com",
     user: "sql7712621",
     database: "sql7712621",
     password: "lnzSk48cVz",
     port: 3306
 });
-db1.connect();
+db.connect();
 const __dirname=dirname(fileURLToPath(import.meta.url));
 const port=3000;
+const saltRounds=10;
 const app=express();
+var books=[];
 var response="";
 
-app.use(express.static('assets'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+try
+{
+    db.query("SELECT title from `TABLE 1`",function (err,result){
+        if(err) throw err;
+        if(result.length>0)
+        {
+            for(var i=0;i<result.length;i++)
+            {
+                books.push(result[i].title);
+            }
+        }
+    });
+}
+catch(err)
+{
+    console.error("Error: ",err);
+}
 app.get("/",(req,res)=>{
+    res.render(__dirname+"/views/open.ejs",{
+        books: books,
+    });
+});
+app.get("/login",(req,res)=>{
     res.render(__dirname+"/views/login.ejs",{
         response: response,
     });
-});
-
+})
+app.get("/signup",(req,res)=>{
+    res.render("signup.ejs");
+})
 app.post("/login",async (req,res) =>{
     const username=req.body.Username;
     const password=req.body.Password;
     try
     {
-        db1.query("SELECT password FROM users WHERE username=?",[username],function(err,result){
+        db.query("SELECT password FROM users WHERE username=?",[username],function(err,result){
             if(err) throw err;
             if(result.length>0)
             {
                 const pass=result[0].password;
-                if(password===pass)
-                {
-                    response="Login Successful.";
-                    res.redirect("/");
-                }
-                else{
-                    response="Invalid Credentials. Try Again.";
-                    res.redirect("/");
-                }
+                bcrypt.compare(password,pass,(err,result)=>{
+                    if(err)
+                    console.error("Error: ",err);
+                    else
+                    {
+                        if(result)
+                        {
+                            response="Login Successful";
+                            res.redirect("/search");
+                        }
+                        else
+                        {
+                            response="Invalid Credentials. Try Again.";
+                            res.redirect("/login");
+                        }
+                    }
+                });
             }
             else
             {
                 response="User does not exist.";
-                res.redirect("/");
+                res.redirect("/login");
             }
         });
     }
@@ -58,7 +91,51 @@ app.post("/login",async (req,res) =>{
         console.error("Error: ",err.message);
         res.redirect("/");
     }
-})
+});
+app.post("/signup",async (req,res)=>{
+    const username=req.body.mail;
+    const password=req.body.pswd;
+    try
+    {
+        db.query("SELECT * FROM users WHERE username=?",[username], function(err,result){
+            if(err) throw err;
+            if(result.length>0)
+            {
+                res.render(__dirname+"/views/signup.ejs",{
+                    alert: "User already exist. Try logging in.",
+                });
+            }
+            else
+            {
+                bcrypt.hash(password,saltRounds,async (err,hash)=>{
+                    if(err)
+                    console.error("Error: ",err.message);
+                    else
+                    {
+                        try
+                        {
+                            db1.execute("INSERT INTO users(username,password) VALUES (?,?)",[username,hash]);
+                            res.redirect("/search")
+                        }
+                        catch(err)
+                        {
+                            console.error("Error: ",err);
+                        }
+                    }
+                });
+            }
+        });
+    }
+    catch(err)
+    {
+        console.error("Error: ",err);
+    }
+});
+app.get("/search",(req,res)=>{
+    res.render(__dirname+"/views/search.ejs",{
+        books: books,
+    });
+});
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}.`)
 });
