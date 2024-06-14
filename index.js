dde5e0dec0f015e06a6ab8534dedf6b54eab5095
NidhishDoshi <140102883+NidhishDoshi@users.noreply.github.com>
2024-06-14 18:21:45 +0530

    Solved disconnect error

--- index.js
+++ index.js
@@ -2,47 +2,51 @@ import express from "express";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-<<<<<<< HEAD
-import pg from "pg";
-import env from "dotenv";
-import mysql from "mysql2";
-
-env.config();
-const Database = mysql.createConnection({
-=======
 import mysql from "mysql2";
 import bcrypt from "bcrypt";
 
 const db=mysql.createConnection({
->>>>>>> df03e2c809fdd7f376d7190221ead2c9e87e7496
     host: "sql7.freesqldatabase.com",
     user: "sql7712621",
     database: "sql7712621",
     password: "lnzSk48cVz",
     port: 3306
 });
-<<<<<<< HEAD
-Database.connect();
-=======
-db.connect();
->>>>>>> df03e2c809fdd7f376d7190221ead2c9e87e7496
+db.connect((err)=>{
+    if(err)
+    console.error("Error connecting to the database: ",err);
+});
+db.on('error',(err)=>{
+    console.error("Database Error: ",err);
+    if(err.code=='ECONNRESET')
+    handleDisconnect()
+});
+function handleDisconnect()
+{
+    db.connect((err)=>{
+        if(err)
+        {
+            console.error("Error reconnecting: ",err);
+            setTimeout(handleDisconnect,2000);
+        }
+        else
+        console.log("Reconnected");
+    });
+}
 const __dirname=dirname(fileURLToPath(import.meta.url));
 const port=3000;
 const saltRounds=10;
 const app=express();
 var books=[];
 var response="";
-<<<<<<< HEAD
-
-=======
->>>>>>> df03e2c809fdd7f376d7190221ead2c9e87e7496
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 try
 {
     db.query("SELECT title from `TABLE 1`",function (err,result){
-        if(err) throw err;
+        if(err)
+        console.error("Error: ",err);
         if(result.length>0)
         {
             for(var i=0;i<result.length;i++)
@@ -74,21 +78,9 @@ app.post("/login",async (req,res) =>{
     const password=req.body.Password;
     try
     {
-<<<<<<< HEAD
-        const result= await Database.query("SELECT password FROM users WHERE name=$1",[username]);
-        const pass=result.rows[0].password;
-        if(password===pass)
-        {
-            response="Login Successful.";
-            res.redirect("/");
-        }
-        else{
-            response="INVALID CREDENTIALS!!. Try Again.";
-            res.redirect("/");
-        }
-=======
         db.query("SELECT password FROM users WHERE username=?",[username],function(err,result){
-            if(err) throw err;
+            if(err)
+            console.error("Error: ",err);
             if(result.length>0)
             {
                 const pass=result[0].password;
@@ -116,7 +108,6 @@ app.post("/login",async (req,res) =>{
                 res.redirect("/login");
             }
         });
->>>>>>> df03e2c809fdd7f376d7190221ead2c9e87e7496
     }
     catch(err){
         console.error("Error: ",err.message);
@@ -129,7 +120,8 @@ app.post("/signup",async (req,res)=>{
     try
     {
         db.query("SELECT * FROM users WHERE username=?",[username], function(err,result){
-            if(err) throw err;
+            if(err)
+            console.log("Error: ",err);
             if(result.length>0)
             {
                 res.render(__dirname+"/views/signup.ejs",{
@@ -166,6 +158,12 @@ app.get("/search",(req,res)=>{
     res.render(__dirname+"/views/search.ejs",{
         books: books,
     });
+});
+app.get("/terms_and_conditions",(req,res)=>{
+    res.render(__dirname+"/views/terms_and_conditions.ejs");
+});
+app.get("/privacy_policy",(req,res)=>{
+    res.render(__dirname+"/views/privacy_policy.ejs");
 });
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}.`)
